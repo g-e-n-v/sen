00a570ca7565faa3e3fc6a09cd250ba88941cd45
spartan-lucle <luc.le@c0x12c.com>
2024-09-20 15:03:34 +0700

    feat: setup example app

---
+++ apps/example/eslint.config.js
@@ -0,0 +1,45 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import perfectionist from "eslint-plugin-perfectionist";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      perfectionist,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          type: "alphabetical",
+          internalPattern: ["@/**"],
+          groups: [
+            "type",
+            ["builtin", "external"],
+            "internal-type",
+            "internal",
+            ["parent-type", "sibling-type", "index-type"],
+            ["parent", "sibling", "index"],
+            "object",
+            "unknown",
+          ],
+        },
+      ],
+      "perfectionist/sort-exports": ["error", { type: "alphabetical" }],
+    },
+  }
+);
